ade344402e158f0a5d27dc35f69a4fc27f5ae4d2
itsmeinsane2265 <kasulaprashant544@gmail.com>
2022-07-29 20:52:19 +0545

    files modified

--- JS/external.js
+++ JS/external.js
@@ -1,7 +1,4 @@
 
-
-
-	
 	var a=1;
 	var b=2;
 	var c= a+b;
@@ -21,3 +18,56 @@
 		/*document.getElementById("current_date").style.color = "red";*/
 		par.style.color = "red";
 	}
+
+
+
+	/*getting Email From Form*/
+
+	function getEmail()
+	{
+		var email = document.getElementById("email").value;
+		console.log(email);
+		document.getElementById("your_email").innerHTML = "Your E-mail is : " +email;
+
+	}
+
+	/*getting Username From Form*/
+	function getUsername()
+	{
+		var username = document.getElementById("username").value;
+		console.log(username)
+		document.getElementById("your_username").innerHTML = "Your Username is : " +username;
+
+	}
+
+
+	/*getting Password From Form*/
+	function getPassword()
+		{
+			var password= document.getElementById("password").value;
+			console.log(password)
+			document.getElementById("your_password").innerHTML = "Your Password is : " +password;
+
+		}
+
+
+
+	/*getting DOB From Form*/
+	function getDOB()
+	{
+		var date_of_birth= document.getElementById("date_of_birth").value;
+		console.log(date_of_birth)
+		document.getElementById("your_dob").innerHTML = "Your DOB is : " +date_of_birth;
+
+	}
+
+	/*getting role From Form*/
+	function getRole()
+	{
+		var role= document.getElementById("role").value;
+		console.log(role)
+		document.getElementById("your_role").innerHTML = "Your role is : " +role;
+
+	}
+
+
